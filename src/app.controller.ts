568f601a57f6bae97d1988a7309e32bb9c800bbf
priyankambliya <priyankambliya18@gmail.com>
2023-09-13 23:02:55 +0530

    update file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/signup')
+  @Render('signup')
+  signUpPage() {
+    return { title: "Signup" };
+  }
+
+  @Get('/login')
+  @Render('login')
+  loginPAge() {
+    return { title: "Login" };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+  await app.listen(3333, () => console.log("Server listning on port:3333.."));
 }
 bootstrap();

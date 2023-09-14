daab4d6939fea204f873275fc97580855a0edd64
priyankambliya <priyankambliya18@gmail.com>
2023-09-14 18:53:33 +0530

    modify file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Request, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { join } from 'path';
+import { User } from './schemas/user.schema';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
@@ -16,5 +17,13 @@ export class AppController {
   @Render('login')
   loginPAge() {
     return { title: "Login" };
+  }
+
+  @UseGuards(AuthGuard("local"))
+  @Post('auth/login')
+  @Render("home")
+  async login(@Request() req, @Body() user: User) {
+    if (!req.user) throw new UnauthorizedException("user not found..")
+    return { title: "home" }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest-rest-api'),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
